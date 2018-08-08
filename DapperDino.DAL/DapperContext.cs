d8db76da9976b4687c4fc93c86831e65f760dac9
Mick Rustenhoven <rustenhovenmick@outlook.com>
2018-08-08 02:00:54 +0200

    Started on DAL

---
+++ DapperDino.DAL/DapperContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperDino.DAL
+{
+    public class DapperContext:DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ DapperDino.DAL/Models/FAQ.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DapperDino.DAL.Models
+{
+    public class FAQ
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public FAQQuestion Question { get; set; }
+        public FAQAnswer Answer { get; set; }
+    }
+    
+
+    public class FAQQuestion
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        // We might want to add a stack trace message
+        public string StackTrace { get; set; }
+        public IEnumerable<string> ErrorLinks { get; set; }
+    }
+
+    public class FAQAnswer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Solution { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<string> UsefulLinks { get; set; }
+    }
+}
