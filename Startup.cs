9bc7f1b5e2d1918534605f56e7c4858cd0fe7475
Mick Rustenhoven <rustenhovenmick@outlook.com>
2018-08-14 21:33:06 +0200

    Reconfigured project

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DapperDino.Dal;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using DapperDino.Data;
 using DapperDino.Models;
 using DapperDino.Services;
 
