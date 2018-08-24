ea407fc64e76dec97f9c80d429defdac60e3b5a7
Mick Rustenhoven <rustenhovenmick@outlook.com>
2018-08-24 03:03:25 +0200

    Start on showing tickets on the website

---
+++ Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DapperDino.DAL.Models;
+using DapperDino.Models.TicketViewModels;
+
+namespace DapperDino.Profiles
+{
+    public class TicketProfile:Profile
+    {
+        public TicketProfile()
+        {
+            CreateMap<Ticket, TicketViewModel>();
+            CreateMap<TicketReaction, TicketReactionViewModel>()
+
+        }
+    }
+}
